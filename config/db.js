37a302e9870ca70a1dabe57f682fe0a8b0ce4289
Dan Spencer <dspence180@gmail.com>
2021-03-26 13:20:42 -0500

    Modified DB connection to work with heroku postgres

--- config/db.js
+++ config/db.js
@@ -5,10 +5,10 @@ const knex = require('knex');
 const db = knex({
     client: 'pg',
     connection: {
-        host : '127.0.0.1',
-        user : 'postgres',
-        password : 'admin',
-        database : 'void-notes'
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
     }
 });
 
